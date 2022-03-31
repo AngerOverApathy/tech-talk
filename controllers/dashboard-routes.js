40271e5511499ba24c5ecb47fc7739fa1ad3ed33
AngerOverApathy <angersavestheworld@gmail.com>
2022-03-31 15:01:50 -0700

    created edit post route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -40,6 +40,45 @@ router.get('/', withAuth, (req, res) => {
       })
   })
 
-  //render edit post page for user
+  //route - edit post page for user
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'post_text',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' })
+          return;
+        }
+        const post = dbPostData.get({ plain: true }) //serialize data
+        res.render('edit-post', { post, loggedIn: true }); //render edit post
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+      });
+  });
 
-  //render 
+  //route - edit user

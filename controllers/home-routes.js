edb4120854de6b137c3f8e59f26a38e9233df721
AngerOverApathy <angersavestheworld@gmail.com>
2022-03-31 14:08:31 -0700

    created homepage and post render functions

---
+++ controllers/home-routes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router()
+const sequelize = require('../config/connection')
+const { Post, User, Comment } = require('../models')
+
+//homepage route render
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            'post_content'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    const posts = dbPostData.map(post => post.get({ plain: true })); //creates post array
+    res.render('homepage', {  //render posts on homepage
+        posts,
+        loggedIn: req.session.loggedIn
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})

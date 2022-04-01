9a9b50c3869694131ccaa79f0a7324013dc7acbe
AngerOverApathy <angersavestheworld@gmail.com>
2022-03-31 18:55:13 -0700

    updated helpers

--- models/Users.js
+++ models/Users.js
@@ -2,3 +2,51 @@ const sequelize = require('../config/connection')
 const bcrypt = require('bcrypt')
 
 const { Model, DataTypes } = require('sequelize')
+
+class User extends Model {
+    //method to run on a user instance to check the password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+      id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true, //this column is the primary key
+          autoIncrement: true
+          },
+          username: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            notEmpty: true,
+            }
+          },
+          email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true, //disallows duplicate
+          validate: {
+              isEmail: true
+              }
+          },
+          password: {
+          type: DataTypes.STRING,
+           allowNull: false,
+          validate: {
+              len: [5]
+              }
+          }
+    },
+    {
+        hooks: {
+            async beforeCreate(newUser) {
+                newUser.password = await bcrypt.hash(newUser.password, 10)
+                return newUser;
+            }
+        }
+    }
+)
